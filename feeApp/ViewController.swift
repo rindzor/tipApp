997a2791d51e3e252c943ca423bb7a9289ef832c
Vladimir Drozdin <rindzorcoop@gmail.com>
2020-03-11 08:09:57 +0200

    Initial Commit

---
+++ feeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  feeApp
+//
+//  Created by  mac on 3/11/20.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
